491c7fb2a28c53a8cbd0e23b805fd681bb5160f1
mihaivarga <me_shuv@yahoo.com>
2014-09-12 11:22:24 +0300

    Site bootstrap

--- libraries/debug/import.php
+++
@@ -1,4 +0,0 @@
-<?php
-	global $page;
-	$this->import('file',dirname(__FILE__).'/dBug.php');
-?>
--- libraries/wbl_admin/import.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once dirname(__FILE__).'/lib/SimpleExcel.php';
-	require_once dirname(__FILE__).'/lib/AdminPage.php';
-	require_once dirname(__FILE__).'/lib/AjaxTable.php';
-	require_once dirname(__FILE__).'/lib/Form.php';
-	require_once dirname(__FILE__).'/lib/Generator.php';
-	require_once dirname(__FILE__).'/lib/Parser.php';
-?>
