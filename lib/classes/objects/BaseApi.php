b8d6642468e898d503db0978159140941277ceb3
mihaivarga <me_shuv@yahoo.com>
2015-04-02 12:13:43 +0300

    Fixed #19 Fixed #15

---
+++ lib/classes/objects/BaseApi.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Api Model Class
+ */
+
+/**
+ * Api Model Class - Api Model Object to be extended in components
+ *
+ * @package WebLauncher\Objects
+ */
+class BaseApi extends Base {
+    /**
+     * Error method
+     * @param string $message 
+     * @param int $code
+     */
+    public function error($message, $code = 1) {
+        return array(
+            'error' => $code,
+            'message' => $message
+        );
+    }
+
+    /**
+     * Api GET method 
+     * Returns one element if id provided or all elements from db
+     * @param array $url_params Pass data from url like REST
+     */
+    public function api_get($url_params) {
+        if (isset_or($url_params[0]))
+            return $this -> get($url_params[0]);
+        return $this -> get_all();
+    }
+
+    /**
+     * Api DELETE method
+     * Deletes the row identified by the provided ID
+     * @param array $url_params Pass data from url like REST
+     */
+    public function api_delete($url_params) {
+        $this -> delete($url_params[0]);
+        return 1;
+    }
+
+    /**
+     * Api PUT method
+     * Updates the row identified by the provided ID
+     * @param array $params Data provided as JSON in php://input
+     * @param array $url_params Data provided in the url as REST
+     * @return array The updated row
+     */
+    public function api_put($params, $url_params) {
+        $id = isset_or($params[$this -> id_field]);
+        if (isset_or($url_params[0]))
+            $id = $url_params[0];
+        $params[$this -> id_field] = $id;
+        $this -> update($params);
+        return $this -> get($params[$this -> id_field]);
+    }
+
+    /**
+     * Api POST method
+     * Inserts or updates the row into db (updates if the ID is provided)
+     * @param array $params Data provided as JSON in php://input
+     * @param array $url_params Data provided in the url as REST
+     * @return array The inserted/updated row
+     */
+    public function api_post($params, $url_params) {
+        $id = isset_or($params[$this -> id_field]);
+        if (isset_or($url_params[0]))
+            $id = $url_params[0];
+        $params[$this -> id_field] = $id;
+        $id = $this -> save($params);
+        return $this -> get($id);
+    }
+}
+?>
---
+++ lib/classes/objects/PageApi.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Api Controller Class
+ */
+
+/**
+ * PageApi Class - Component/Controller Api Object to be extended in components
+ *
+ * @package WebLauncher\Objects
+ */
+class PageApi extends Page {
+    var $model = '';
+    var $method = 'get';
+    var $params = array();
+
+    /**
+     * On init method
+     */
+    function _on_init() {
+        parent::_on_init();
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: accept, content-type, authorization");
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header("Access-Control-Allow-Headers: accept, content-type, authorization");
+            header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE');
+            die ;
+        }
+    }
+
+    /**
+     * On load method
+     */
+    function _on_load() {
+        header("Content-Type: application/json");
+        $this -> model = $this -> system -> subquery[1];
+        $this -> method = strtolower($_SERVER['REQUEST_METHOD']);
+        if (isset($this -> system -> subquery[2]) && $this -> system -> subquery[2])
+            $this -> params = array_slice($this -> system -> subquery, 2);
+        if (method_exists($this, 'api_' . $this -> method))
+            $this -> {'api_'.$this->method}();
+        parent::_on_load();
+    }
+
+    /**
+     * Api get method
+     */
+    function api_get() {
+        $return = '';
+        $return = $this -> models -> {$this -> model} -> api_get($this -> params);
+        die(json_encode($return, JSON_NUMERIC_CHECK));
+    }
+
+    /**
+     * Api delete method
+     */
+    function api_delete() {
+        $return = '';
+        $return = $this -> models -> {$this -> model} -> api_delete($this -> params);
+        die(json_encode($return, JSON_NUMERIC_CHECK));
+    }
+
+    /**
+     * Api put method
+     */
+    function api_put() {
+        $return = '';
+        $return = $this -> models -> {$this -> model} -> api_put($this -> get_json(), $this -> params);
+        die(json_encode($return, JSON_NUMERIC_CHECK));
+    }
+
+    /**
+     * Api post method
+     */
+    function api_post() {
+        $return = '';
+        $return = $this -> models -> {$this -> model} -> api_post($this -> get_json(), $this -> params);
+        die(json_encode($return, JSON_NUMERIC_CHECK));
+    }
+
+    /**
+     * Get json from php://input
+     */
+    function get_json() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+}
+?>
