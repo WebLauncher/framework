fcf6ba30b5df2e2323eaaf8b1d4e5b1e113d8d08
mihaivarga <me_shuv@yahoo.com>
2015-07-22 14:57:16 +0300

    cleanup and backwardcompatibility

--- config.php
+++ config.php
@@ -1,23 +1,34 @@
 <?php
-// Add config files to the system this will load extra config in case hostnames or
-// callback function are matched.
-// Add 'console' as hostname if that configuration should be loaded when script is run
-// from console.
-// By default 'development' is set to array('localhost','127.0.0.1') so there no need
-// to add it.
-// $this -> add_config('development', 'localhost');
+/*
+	Add config files to the system this will load extra config in case hostnames or
+	callback function are matched.
+
+	Add 'console' as hostname if that configuration should be loaded when script is run
+	from console.
+
+	By default 'development' is set to array('localhost','127.0.0.1') so there no need
+	to add it.
+
+	$this -> add_config('development', 'localhost');
+*/
 
 $this->add_config('production', 'example.com');
 $this->add_config('development', 'localhost:10001');
 
-/* Use to import CSS files into template
- $page->add_css_file('{$skin_styles}screen.css');
- $page->add_css_file('{$skin_styles}ui.css');
- $page->add_css_file('{$skin_styles}ie.css','text/css','screen, projection','if IE');
- */
+/*
+	Use to import CSS files into template
+
+	$page->add_css_file('{$skin_styles}screen.css');
+ 	$page->add_css_file('{$skin_styles}ui.css');
+ 	$page->add_css_file('{$skin_styles}ie.css','text/css','screen, projection','if IE');
+*/
 $this->add_css_file('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css');
 
-/* Use to import JS files into template */
+/* 
+	Use to import JS files into template 
+
+	$this->add_js_file('{$root_scripts}jquery/jquery.js');
+*/
 $this->add_js_file('{$root_scripts}jquery/jquery.js');
 $this->add_js_file('{$root_scripts}validation/jquery.validate.js');
 $this->add_js_file('{$root_scripts}validation/jquery.validate-ext.js');
